adafb79cddd192f141db0dbc08d7627d17c407ed
Seu Nome <seu@email.com>
2025-08-07 16:23:58 -0300

    ˜Finalizando˜

---
+++ frontJsonServer/main.js
@@ -0,0 +1,31 @@
+"use strict";
+
+    const containerFotos = document.getElementById("containerFotos");
+
+    async function pegarFotos() {
+      const response = await fetch("https://jsonserver-apifake-1.onrender.com/fotos");
+      const data = await response.json();
+      return data;
+    }
+
+    async function carregarFotos() {
+      const fotos = await pegarFotos();
+      let index = 0;
+      setInterval(() => {
+        containerFotos.replaceChildren();
+
+        const foto = document.createElement("img");
+        foto.src = fotos[index].imagem;
+        foto.alt = fotos[index].legenda;
+
+        const legenda = document.createElement("p");
+        legenda.textContent = fotos[index].legenda;
+
+        containerFotos.appendChild(foto);
+        containerFotos.appendChild(legenda);
+
+        index = (index + 1) % fotos.length;
+      }, 4000);
+    }
+
+    carregarFotos();
